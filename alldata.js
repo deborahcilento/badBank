de37368c74b70bcbc6deb18070233fe07b2bd0bf
deborah <debbiecilento@gmail.com>
2024-03-17 22:11:55 -0600

    style

--- alldata.js
+++ alldata.js
@@ -2,7 +2,7 @@ function AllData(){
   const ctx = React.useContext(UserContext);
   return (
     <>
-      <h5>All Data in Store</h5>
+      <h5 id = "datatitle">All Data in Store</h5>
       {ctx.users.map(user => (
         <div key={user.email} className="card mb-3">
           <ul className="list-group list-group-flush">
